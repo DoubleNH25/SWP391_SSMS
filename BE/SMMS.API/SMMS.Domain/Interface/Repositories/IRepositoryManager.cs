dd8f66893a0bebadde91cf5671d4f415e2d471da
DoubleNH25 <145324178+DoubleNH25@users.noreply.github.com>
2025-05-26 07:36:37 +0700

    Setup RepositoryBase && RepositoryManager

---
+++ BE/SMMS.API/SMMS.Domain/Interface/Repositories/IRepositoryManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMMS.Domain.Interface.Repositories
+{
+	public interface IRepositoryManager
+	{
+		//Fill with each interface entity repository
+		IUserRepository UserRepository { get; }
+		//Fill with each entity repository
+
+		Task SaveAsync();
+	}
+}
---
+++ BE/SMMS.API/SMMS.Infrastructure/Implements/RepositoryManager.cs
@@ -0,0 +1,29 @@
+﻿using SMMS.Domain.Interface.Repositories;
+using SMMS.Infrastructure.Context;
+
+
+namespace SMMS.Infrastructure.Implements
+{
+	public class RepositoryManager : IRepositoryManager
+	{
+		private readonly DatabaseContext _context;
+		private IUserRepository _userRepository;
+
+
+		public RepositoryManager(DatabaseContext context)
+		{
+			_context = context;
+			_userRepository = new UserRepository(context);
+		}
+
+		public IUserRepository UserRepository
+		{
+			get
+			{
+				return _userRepository;
+			}
+		}
+
+		public Task SaveAsync() => _context.SaveChangesAsync();
+	}
+}
