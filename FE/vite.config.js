dcfaad45130599e1fd00d8856332dec4126c9a05
Nguyễn Thanh Thắng <nguyenthanhthang190500@gmail.com>
2025-07-28 23:21:52 +0700

    Merge FE folder from live-deployment branch into main

--- FE/vite.config.js
+++ FE/vite.config.js
@@ -3,22 +3,23 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import svgr from "vite-plugin-svgr";
 export default defineConfig({
-    plugins: [
-        react(),
-        svgr({
-            svgrOptions: {
-                icon: true,
-                exportType: "named",
-                namedExport: "ReactComponent",
-            },
-        }),
-    ],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "src"),
-        },
-    },server: {
-    host: "127.0.0.1", // ✅ Đây là dòng quan trọng để reCAPTCHA hoạt động ổn định
-    port: 5173,         // (Tùy chọn) bạn có thể chỉ định cổng, nếu cần
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        icon: true,
+        exportType: "named",
+        namedExport: "ReactComponent",
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  server: {
+    host: "0.0.0.0", // ✅ Cấu hình để có thể truy cập từ bên ngoài
+    port: 5173, // (Tùy chọn) bạn có thể chỉ định cổng, nếu cần
   },
 });
